bbb223c78dd32f55f0658fb94a7202f8f4770167
Daniel Duarte <danieldd.ar@gmail.com>
2019-09-02 23:33:46 -0300

    Added FlowManager.runFromUrl(...).

--- src/engine/flow-manager.ts
+++ src/engine/flow-manager.ts
@@ -1,4 +1,6 @@
 import * as fs from 'fs';
+import * as http from 'http';
+import * as https from 'https';
 import { GenericValueMap, TaskResolverMap } from '../types';
 import { Flow } from './flow';
 import { FlowSpec } from './specs';
@@ -53,5 +55,66 @@ export class FlowManager {
     });
   }
 
-  // @todo implement runFromUrl
+  public static runFromUrl(
+    flowSpecUrl: string,
+    params: GenericValueMap = {},
+    expectedResults: string[] = [],
+    resolvers: TaskResolverMap = {},
+    context: GenericValueMap = {},
+  ): Promise<GenericValueMap> {
+    let client: any = null;
+    if (flowSpecUrl.startsWith('http://')) {
+      client = http;
+    } else if (flowSpecUrl.startsWith('https://')) {
+      client = https;
+    }
+
+    if (client === null) {
+      let actualProtocol = null;
+      const matchResult = flowSpecUrl.match(/^([a-zA-Z]+):/);
+      if (Array.isArray(matchResult) && matchResult.length === 2) {
+        actualProtocol = matchResult[1];
+      }
+      return Promise.resolve(
+        new Error(
+          `Protocol not supported${
+            actualProtocol ? `: ${actualProtocol}` : ''
+          }. Supported protocols are: [http, https]`,
+        ),
+      );
+    }
+
+    return new Promise<GenericValueMap>((resolveFlow, reject) => {
+      client
+        .get(flowSpecUrl, (res: any) => {
+          const { statusCode } = res;
+          const contentType = res.headers['content-type'] || 'application/json';
+
+          let error;
+          if (statusCode !== 200) {
+            error = new Error(`Request failed with status code: ${statusCode}`);
+          } else if (!(/^application\/json/.test(contentType) || /^text\/plain/.test(contentType))) {
+            error = new Error(
+              `Invalid content-type. Expected application/json or text/plain but received ${contentType}`,
+            );
+          }
+
+          if (error) {
+            reject(error);
+          } else {
+            res.setEncoding('utf8');
+            let rawData = '';
+            res.on('data', (chunk: any) => {
+              rawData += chunk;
+            });
+            res.on('end', () => {
+              FlowManager.runFromString(rawData, params, expectedResults, resolvers, context).then(resolveFlow, reject);
+            });
+          }
+        })
+        .on('error', (error: Error) => {
+          reject(error);
+        });
+    });
+  }
 }
---
+++ test/load-from-url.ts
@@ -0,0 +1,81 @@
+import { expect } from 'chai';
+import { FlowManager, GenericValueMap } from '../src';
+
+class TimerResolver {
+  public async exec(): Promise<GenericValueMap> {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve({ a: 1 });
+      }, 200);
+    });
+  }
+}
+
+class DirectResolver {
+  public async exec(): Promise<GenericValueMap> {
+    return { b: 2 };
+  }
+}
+
+describe('can run a flow', () => {
+  it('from a JSON got from a URL', () => {
+    return FlowManager.runFromUrl(
+      'https://raw.githubusercontent.com/daniel-duarte/flowed/master/test/examples/example6.flowed.json',
+      {
+        param1: 'PARAM1',
+        param2: 'PARAM2',
+        param3: 'PARAM3',
+      },
+      ['g1', 'g2'],
+      {
+        timer: TimerResolver,
+        direct: DirectResolver,
+      },
+    );
+  });
+
+  it('from a JSON got from a URL and throw error', async () => {
+    const filepath = 'https://raw.githubusercontent.com/daniel-duarte/flowed/master/test/examples/invented.flowed.json';
+
+    try {
+      await FlowManager.runFromUrl(
+        filepath,
+        {
+          param1: 'PARAM1',
+          param2: 'PARAM2',
+          param3: 'PARAM3',
+        },
+        ['g1', 'g2'],
+        {
+          timer: TimerResolver,
+          direct: DirectResolver,
+        },
+      );
+    } catch (error) {
+      expect(error.message).to.be.eql(`Request failed with status code: 404`);
+    }
+  });
+
+  it('from a JSON got from a URL with invalid format and throw an error', async () => {
+    const filepath =
+      'https://raw.githubusercontent.com/daniel-duarte/flowed/master/test/examples/example6.flowed.json.invalid';
+
+    try {
+      await FlowManager.runFromUrl(
+        filepath,
+        {
+          param1: 'PARAM1',
+          param2: 'PARAM2',
+          param3: 'PARAM3',
+        },
+        ['g1', 'g2'],
+        {
+          timer: TimerResolver,
+          direct: DirectResolver,
+        },
+      );
+    } catch (error) {
+      expect(error.message).to.be.eql('Unexpected token \n in JSON at position 474');
+    }
+  });
+});
