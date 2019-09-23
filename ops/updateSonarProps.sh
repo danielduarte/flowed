59e53a4212029e0dd65f977233bf242fb6e6d962
Daniel Duarte <danieldd.ar@gmail.com>
2019-09-22 23:49:05 -0300

    Script for update SonarQube properties improved

--- ops/updateSonarProps.sh
+++ ops/updateSonarProps.sh
@@ -4,23 +4,15 @@
 # This script parses the project's name and version from its package.json and automagically
 # updates the version and package name in the SonarQube configuration properties file.
 # It can be used as a pre step before running the sonar-scanner command
-# It also creates a backup of the props file with suffix *.bak
+#
 #prerequisites  : NodeJS based project with package.json, sonar*.properties file in the cwd
 #author         : Christian-André Giehl <christian@emailbrief.de>
+#modified by    : Daniel Duarte <danieldd.ar@gmail.com>
 #date           : 20180220
-#version        : 1.0
+#version        : 1.1
 #usage          : sh updateSonarProps.sh
 #==============================================================================
 echo "Updating the SonarQube properties..."
-
-# Get the version from package.json
-PACKAGE_VERSION=$(cat package.json \
-  | grep version \
-  | head -1 \
-  | awk -F: '{ print $2 }' \
-  | sed 's/[",]//g' \
-  | tr -d '[[:space:]]')
-echo "Extracted version: ${PACKAGE_VERSION}"
 
 # Get the project name from package.json
 PACKAGE_NAME=$(cat package.json \
@@ -29,20 +21,27 @@ PACKAGE_NAME=$(cat package.json \
   | awk -F: '{ print $2 }' \
   | sed 's/[",]//g' \
   | tr -d '[[:space:]]')
-echo "Extracted project: ${PACKAGE_NAME}"
+echo "Project: ${PACKAGE_NAME}"
+
+# Get the version from package.json
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+echo "Version: ${PACKAGE_VERSION}"
 
 # Get the Sonar properties file
 SONAR_FILE=$(find ./ -iname sonar*.properties -type f)
-echo "Sonar file found: ${SONAR_FILE}"
+echo "Sonar file: ${SONAR_FILE}"
 
 # Update the version
 REPLACE='^sonar.projectVersion=.*$'
 WITH="sonar.projectVersion=${PACKAGE_VERSION}"
-sed -i.bak "s#${REPLACE}#${WITH}#g" ${SONAR_FILE}
+sed -i "s#${REPLACE}#${WITH}#g" ${SONAR_FILE}
 
 # Update the project name
 REPLACE='^sonar.projectName=.*$'
 WITH="sonar.projectName=${PACKAGE_NAME}"
-sed -i.bak "s#${REPLACE}#${WITH}#g" ${SONAR_FILE}
-
-echo "Done!"
+sed -i "s#${REPLACE}#${WITH}#g" ${SONAR_FILE}
