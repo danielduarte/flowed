2bbde3b23ad62b8e2e04daf0909356700b2d9e27
Daniel Duarte <danieldd.ar@gmail.com>
2019-11-21 22:12:09 -0300

    Removed performance test written as unit test

--- test/performance.ts
+++ test/performance.ts
@@ -1,20 +1,22 @@
-import { debug as rawDebug } from 'debug';
-import * as Examples from './examples/index';
-const debug = rawDebug('flowed:test');
+// @todo create performance test suite and remove this one
 
-const times = 100000;
-const maxTime = '5s'; // @todo Put back to 2s when created performance test suite
-
-describe('a simple flow with 7 tasks', function() {
-  this.timeout(maxTime);
-
-  it(`runs ${times} times in less than ${maxTime} ms`, async () => {
-    const start = Date.now();
-
-    for (let i = 0; i < times; i++) {
-      await Examples.example2();
-    }
-
-    debug(`Ran flow ${times} times in ${Date.now() - start} ms`);
-  });
-});
+// import { debug as rawDebug } from 'debug';
+// import * as Examples from './examples/index';
+// const debug = rawDebug('flowed:test');
+//
+// const times = 100000;
+// const maxTime = '2s'; // @todo Put back to 2s when created performance test suite
+//
+// describe('a simple flow with 7 tasks', function() {
+//   this.timeout(maxTime);
+//
+//   it(`runs ${times} times in less than ${maxTime} ms`, async () => {
+//     const start = Date.now();
+//
+//     for (let i = 0; i < times; i++) {
+//       await Examples.example2();
+//     }
+//
+//     debug(`Ran flow ${times} times in ${Date.now() - start} ms`);
+//   });
+// });
